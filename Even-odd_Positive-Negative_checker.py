e0feb73d72121cd4200c88a51d9f1032c311a83a
Subham Mondal <subhammondal2502@gmail.com>
2026-09-10 07:32:45 +0530

    Update Even-odd_Positive-Negative_checker.py

--- Even-odd_Positive-Negative_checker.py
+++ Even-odd_Positive-Negative_checker.py
@@ -1,14 +1,14 @@
 #Even/odd & Positive/Negative checker........
 number=int(input("choose a number : "))
-if number > 0:
+if number > 0 :
     print("number is positive : ")  
-    if number % 2 == 0:
+    if number % 2 == 0 :
         print("number is even ")
     else:
         print("number is odd")    
-elif number < 0:
+elif number < 0 :
     print("number is nevative : ")
-    if  number % 2 == 0:
+    if  number % 2 == 0 :
         print("number is even ")
     else:
         print("number is odd")    
