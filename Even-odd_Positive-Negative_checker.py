fceedce009216ccd9bf28d321aa64fe65784e544
Subham Mondal <subhammondal2502@gmail.com>
2026-09-12 13:51:56 +0530

    Update Even-odd_Positive-Negative_checker.py

--- Even-odd_Positive-Negative_checker.py
+++ Even-odd_Positive-Negative_checker.py
@@ -5,15 +5,13 @@ if number > 0 :
     if number % 2 == 0 :
         print("number is even ")
     else:
-        print("number is odd")  
-        
+        print("number is odd")         
 elif number < 0 :
     print("number is nevative : ")
     if  number % 2 == 0 :
         print("number is even ")
     else:
-        print("number is odd") 
-        
+        print("number is odd")      
 else:
     print("number is zero") 
     
